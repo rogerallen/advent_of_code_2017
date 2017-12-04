c42b16ee280011fda22184fe46688c359c45894c
Roger Allen <rallen@gmail.com>
2017-12-03 23:07:34 -0800

    day 2 complete
    Learned about Option types.

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,11 @@ fn main() {
     if args.len() != 2 {
         usage();
     }
-    println!("Args: {:?}", &args);
+    //println!("Args: {:?}", &args);
     let day = &args[1];
     match day as &str {
         "1" => { day01(); }
+        "2" => { day02(); }
         _ => { usage(); }
     }
 }
@@ -105,4 +106,114 @@ fn str_to_digits(s: &str) -> Vec<i32> {
     return s.chars()
         .map(|x| x.to_string().parse::<i32>().unwrap())
         .collect();
+}
+
+// ======================================================================
+// Day 2
+// ======================================================================
+
+fn day02() {
+    println!("Day 2\n");
+
+    println!("Part One");
+
+    println!("Testing...");
+    let mut test_input = Vec::new();
+    test_input.push(vec![5, 1,  9, 5]);
+    test_input.push(vec![7, 5, 3]);
+    test_input.push(vec![2, 4,  6, 8]);
+    println!("  {:?}",&test_input);
+    let test_answer = checksum(&test_input);
+    if test_answer != 18 {
+        println!("  test FAIL");
+    } else {
+        println!("  test pass")
+    }
+
+    println!("Solution...");
+
+    let mut real_test_input = Vec::new();
+    real_test_input.push(vec![116,	1259,	1045,	679,	1334,	157,	277,	1217,	218,	641,	1089,	136,	247,	1195,	239,	834]);
+    real_test_input.push(vec![269,	1751,	732,	3016,	260,	6440,	5773,	4677,	306,	230,	6928,	7182,	231,	2942,	2738,	3617]);
+    real_test_input.push(vec![644,	128,	89,	361,	530,	97,	35,	604,	535,	297,	599,	121,	567,	106,	114,	480]);
+    real_test_input.push(vec![105,	408,	120,	363,	430,	102,	137,	283,	123,	258,	19,	101,	181,	477,	463,	279]);
+    real_test_input.push(vec![873,	116,	840,	105,	285,	238,	540,	22,	117,	125,	699,	953,	920,	106,	113,	259]);
+    real_test_input.push(vec![3695,	161,	186,	2188,	3611,	2802,	157,	2154,	3394,	145,	2725,	1327,	3741,	2493,	3607,	4041]);
+    real_test_input.push(vec![140,	1401,	110,	119,	112,	1586,	125,	937,	1469,	1015,	879,	1798,	122,	1151,	100,	926]);
+    real_test_input.push(vec![2401,	191,	219,	607,	267,	2362,	932,	2283,	889,	2567,	2171,	2409,	1078,	2247,	2441,	245]);
+    real_test_input.push(vec![928,	1142,	957,	1155,	922,	1039,	452,	285,	467,	305,	506,	221,	281,	59,	667,	232]);
+    real_test_input.push(vec![3882,	1698,	170,	5796,	2557,	173,	1228,	4630,	174,	3508,	5629,	4395,	180,	5100,	2814,	2247]);
+    real_test_input.push(vec![396,	311,	223,	227,	340,	313,	355,	469,	229,	162,	107,	76,	363,	132,	453,	161]);
+    real_test_input.push(vec![627,	1331,	1143,	1572,	966,	388,	198,	2068,	201,	239,	176,	1805,	1506,	1890,	1980,	1887]);
+    real_test_input.push(vec![3390,	5336,	1730,	4072,	5342,	216,	3823,	85,	5408,	5774,	247,	5308,	232,	256,	5214,	787]);
+    real_test_input.push(vec![176,	1694,	1787,	1586,	3798,	4243,	157,	4224,	3603,	2121,	3733,	851,	2493,	4136,	148,	153]);
+    real_test_input.push(vec![2432,	4030,	3397,	4032,	3952,	2727,	157,	3284,	3450,	3229,	4169,	3471,	4255,	155,	127,	186]);
+    real_test_input.push(vec![919,	615,	335,	816,	138,	97,	881,	790,	855,	89,	451,	789,	423,	108,	95,	116]);
+    let test_answer = checksum(&real_test_input);
+    println!("  answer = {:?}",&test_answer);
+
+    println!("\nPart Two");
+
+    println!("Testing...");
+    let mut test_input = Vec::new();
+    test_input.push(vec![5, 9, 2, 8]);
+    test_input.push(vec![9, 4, 7, 3]);
+    test_input.push(vec![3, 8, 6, 5]);
+    println!("  {:?}",&test_input);
+    let test_answer = checksum2(&test_input);
+    if test_answer != 9 {
+        println!("  test FAIL");
+    } else {
+        println!("  test pass")
+    }
+
+    println!("Solution...");
+    let test_answer = checksum2(&real_test_input);
+    println!("  answer = {:?}",&test_answer);
+
+}
+
+fn checksum(rows: &Vec<Vec<i32>>) -> i32 {
+    let mut sum = 0;
+    for row in rows {
+        sum += mmdiff(&row);
+    }
+    return sum;
+}
+
+fn mmdiff(row: &Vec<i32>) -> i32 {
+    let mx;
+    let mn;
+    match row.iter().max() {
+        None => { println!{"unhandled error!"}; return -1; }
+        Some(i) => { mx = *i; }
+    }
+    match row.iter().min() {
+        None => { println!{"unhandled error!"}; return -1; }
+        Some(i) => { mn = *i; }
+    }
+    return mx - mn;
+}
+
+fn checksum2(rows: &Vec<Vec<i32>>) -> i32 {
+    let mut sum = 0;
+    for row in rows {
+        sum += div2val(&row);
+    }
+    return sum;
+}
+
+// "find the only two numbers in each row where one evenly divides the other"
+fn div2val(row: &Vec<i32>) -> i32 {
+    for (i,x) in row.iter().enumerate() {
+        for (j,y) in row.iter().enumerate() {
+            if i != j {
+                if x % y == 0 {
+                    // going to assume first match is all we need
+                    return x/y;
+                }
+            }
+        }
+    }
+    return -1;
 }
