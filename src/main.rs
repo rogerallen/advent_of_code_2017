1636c34c67842adca733a73a990f9fdb400d5ce1
Roger Allen <rallen@gmail.com>
2017-12-13 23:26:30 -0800

    fixed all clippy errors
    installed nightly rustup and did
    rm clippy.log && cargo clippy > clippy.log 2>&1

--- src/main.rs
+++ src/main.rs
@@ -51,19 +51,19 @@ fn day01() {
     let test_strings = vec![ "1122", "1111", "1234", "91212129"];
     let test_answers = vec![ 3, 4, 0, 9];
     for (i,s) in test_strings.iter().enumerate() {
-        let a = captcha(&s);
+        let a = captcha(s);
         let a_correct = test_answers[i];
         print!("  captcha({:?}) == {:?}",&s,&a);
         if a != a_correct {
             println!(" FAIL");
         } else {
-            println!("");
+            println!();
         }
     }
 
     println!("Solution...");
     let test_input = "9513446799636685297929646689682997114316733445451534532351778534251427172168183621874641711534917291674333857423799375512628489423332297538215855176592633692631974822259161766238385922277893623911332569448978771948316155868781496698895492971356383996932885518732997624253678694279666572149831616312497994856288871586777793459926952491318336997159553714584541897294117487641872629796825583725975692264125865827534677223541484795877371955124463989228886498682421539667224963783616245646832154384756663251487668681425754536722827563651327524674183443696227523828832466473538347472991998913211857749878157579176457395375632995576569388455888156465451723693767887681392547189273391948632726499868313747261828186732986628365773728583387184112323696592536446536231376615949825166773536471531487969852535699774113163667286537193767515119362865141925612849443983484245268194842563154567638354645735331855896155142741664246715666899824364722914296492444672653852387389477634257768229772399416521198625393426443499223611843766134883441223328256883497423324753229392393974622181429913535973327323952241674979677481518733692544535323219895684629719868384266425386835539719237716339198485163916562434854579365958111931354576991558771236977242668756782139961638347251644828724786827751748399123668854393894787851872256667336215726674348886747128237416273154988619267824361227888751562445622387695218161341884756795223464751862965655559143779425283154533252573949165492138175581615176611845489857169132936848668646319955661492488428427435269169173654812114842568381636982389224236455633316898178163297452453296667661849622174541778669494388167451186352488555379581934999276412919598411422973399319799937518713422398874326665375216437246445791623283898584648278989674418242112957668397484671119761553847275799873495363759266296477844157237423239163559391553961176475377151369399646747881452252547741718734949967752564774161341784833521492494243662658471121369649641815562327698395293573991648351369767162642763475561544795982183714447737149239846151871434656618825566387329765118727515699213962477996399781652131918996434125559698427945714572488376342126989157872118279163127742349";
-    println!("  answer is {:?}",captcha(&test_input));
+    println!("  answer is {:?}",captcha(test_input));
 
     println!("\nPart Two");
 
@@ -71,23 +71,23 @@ fn day01() {
     let test_strings = vec![ "1212", "1221", "123425", "123123", "12131415"];
     let test_answers = vec![     6,      0,        4,       12,          4];
     for (i,s) in test_strings.iter().enumerate() {
-        let a = captcha2(&s);
+        let a = captcha2(s);
         let a_correct = test_answers[i];
         print!("  captcha2({:?}) == {:?}",&s,&a);
         if a != a_correct {
             println!(" FAIL");
         } else {
-            println!("");
+            println!();
         }
     }
 
     println!("Solution...");
-    println!("  answer is {:?}",captcha2(&test_input));
+    println!("  answer is {:?}",captcha2(test_input));
 
 }
 
 fn captcha(s: &str) -> i32 {
-    let s_digits = str_to_digits(&s);
+    let s_digits = str_to_digits(s);
     //println!("{:?}",&s_digits);
     let mut sum_digits = Vec::new();
     for (i,x) in s_digits.iter().enumerate() {
@@ -96,12 +96,11 @@ fn captcha(s: &str) -> i32 {
             sum_digits.push(*x);
         }
     }
-    let sum = sum_digits.iter().sum();
-    return sum;
+    sum_digits.iter().sum()
 }
 
 fn captcha2(s: &str) -> i32 {
-    let s_digits = str_to_digits(&s);
+    let s_digits = str_to_digits(s);
     //println!("{:?}",&s_digits);
     let mut sum_digits = Vec::new();
     for (i,x) in s_digits.iter().enumerate() {
@@ -111,14 +110,13 @@ fn captcha2(s: &str) -> i32 {
             sum_digits.push(*x);
         }
     }
-    let sum = sum_digits.iter().sum();
-    return sum;
+    sum_digits.iter().sum()
 }
 
 fn str_to_digits(s: &str) -> Vec<i32> {
-    return s.chars()
+    s.chars()
         .map(|x| x.to_string().parse::<i32>().unwrap())
-        .collect();
+        .collect()
 }
 
 // ======================================================================
@@ -187,15 +185,15 @@ fn day02() {
 
 }
 
-fn checksum(rows: &Vec<Vec<i32>>) -> i32 {
+fn checksum(rows: &[Vec<i32>] ) -> i32 {
     let mut sum = 0;
     for row in rows {
-        sum += mmdiff(&row);
+        sum += mmdiff(row);
     }
-    return sum;
+    sum
 }
 
-fn mmdiff(row: &Vec<i32>) -> i32 {
+fn mmdiff(row: &[i32]) -> i32 {
     let mx;
     let mn;
     match row.iter().max() {
@@ -206,30 +204,28 @@ fn mmdiff(row: &Vec<i32>) -> i32 {
         None => { println!{"unhandled error!"}; return -1; }
         Some(i) => { mn = *i; }
     }
-    return mx - mn;
+    mx - mn
 }
 
-fn checksum2(rows: &Vec<Vec<i32>>) -> i32 {
+fn checksum2(rows: &[Vec<i32>]) -> i32 {
     let mut sum = 0;
     for row in rows {
-        sum += div2val(&row);
+        sum += div2val(row);
     }
-    return sum;
+    sum
 }
 
 // "find the only two numbers in each row where one evenly divides the other"
-fn div2val(row: &Vec<i32>) -> i32 {
+fn div2val(row: &[i32]) -> i32 {
     for (i,x) in row.iter().enumerate() {
         for (j,y) in row.iter().enumerate() {
-            if i != j {
-                if x % y == 0 {
-                    // going to assume first match is all we need
-                    return x/y;
-                }
+            if (i != j) && (x % y == 0) {
+                // going to assume first match is all we need
+                return x/y;
             }
         }
     }
-    return -1;
+    -1
 }
 
 // ======================================================================
@@ -238,10 +234,10 @@ fn div2val(row: &Vec<i32>) -> i32 {
 const GRID_SIZE: i32 = 26; // be even
 const GRID_SIZE2: i32 = GRID_SIZE * GRID_SIZE;
 fn grid_index(x: &i32, y: &i32) -> usize {
-    return (y*GRID_SIZE + x) as usize;
+    (y*GRID_SIZE + x) as usize
 }
 fn grid_centered_index(x: &i32, y: &i32) -> usize {
-    return grid_index(&(GRID_SIZE/2+x),&(GRID_SIZE/2+y));
+    grid_index(&(GRID_SIZE/2+x),&(GRID_SIZE/2+y))
 }
 
 fn day03() {
@@ -252,25 +248,25 @@ fn day03() {
 
     println!("Testing...");
 
-    for i in vec![1,2,3,4,5,6,7,8].iter() {
-        println!("dist({:?}) = {:?}",&i,spiral_xy(&i));
+    for i in &[1,2,3,4,5,6,7,8] {
+        println!("dist({:?}) = {:?}",&i,spiral_xy(*i));
     }
     let test_inputs = vec![ 1, 12, 23, 1024];
     let test_answers = vec![ 0, 3, 2, 31];
     for (i,s) in test_inputs.iter().enumerate() {
-        let a = manhattan_spiral_distance(&s);
+        let a = manhattan_spiral_distance(*s);
         let a_correct = test_answers[i];
         print!("  manhattan_spiral_distance({:?}) == {:?}",&s,&a);
         if a != a_correct {
             println!(" FAIL");
         } else {
-            println!("");
+            println!();
         }
     }
 
     println!("Solution...");
-    let test_input = 265149;
-    println!("  answer = {:?}",manhattan_spiral_distance(&test_input));
+    let test_input = 265_149;
+    println!("  answer = {:?}",manhattan_spiral_distance(test_input));
 
 
     println!("Part Two");
@@ -280,24 +276,24 @@ fn day03() {
     let mut my_grid: [i32; GRID_SIZE2 as usize] = [0; GRID_SIZE2 as usize];
     my_grid[grid_centered_index(&0,&0)] = 1;
     for i in 2..24 {
-        let (x,y) = spiral_xy(&i);
+        let (x,y) = spiral_xy(i);
         my_grid[grid_centered_index(&x,&y)] = neighbor_sum(&my_grid,&(GRID_SIZE/2+x),&(GRID_SIZE/2+y));
     }
     for y in (10..17).rev() {
         for x in 10..17 {
             print!("{:5} ",&my_grid[grid_index(&x,&y)]);
         }
-        println!("");
+        println!();
     }
 
     println!("Solution...");
     let mut my_grid: [i32; GRID_SIZE2 as usize] = [0; GRID_SIZE2 as usize];
     my_grid[grid_centered_index(&0,&0)] = 1;
     for i in 2..GRID_SIZE2/2 {
-        let (x,y) = spiral_xy(&i);
+        let (x,y) = spiral_xy(i);
         let val = neighbor_sum(&my_grid,&(GRID_SIZE/2+x),&(GRID_SIZE/2+y));
         my_grid[grid_centered_index(&x,&y)] = val;
-        if val > 265149 {
+        if val > 265_149 {
             println!("wrote value={} on square={}",&val,&i);
             return;
         }
@@ -313,29 +309,29 @@ fn day03() {
 // x(n) = -1^(m+1) [ (n - m(m+1)) * [floor(2*sqrt(n)) is even] + ceil(m / 2) ]
 // y(n) = -1^(m+1) [ (n - m(m+1)) * [floor(2*sqrt(n)) is odd] - ceil(m / 2) ]
 // Formula adjusted below (I 'cheated' a bit and played with python to figure this out)
-fn spiral_xy(n0: &i32) -> (i32, i32) {
+fn spiral_xy(n0: i32) -> (i32, i32) {
     let n = n0 - 1;
-    let m = (n as f64).sqrt().floor() as i32;
-    let fsn = (2.0*(n as f64).sqrt()).floor() as i32;
+    let m = f64::from(n).sqrt().floor() as i32;
+    let fsn = (2.0*f64::from(n).sqrt()).floor() as i32;
     let fsn_is_even = 1-(fsn % 2);
     let fsn_is_odd = fsn % 2;
     let x = (-1 as i32).pow((m) as u32) * ( ((n - m*(m+1)) * fsn_is_odd)  - ((m as f32 / 2.0).ceil() as i32) );
     let y = (-1 as i32).pow((m+1) as u32) * ( ((n - m*(m+1)) * fsn_is_even) + ((m as f32 / 2.0).ceil() as i32) );
     //println!("{} {} {} {} {} {} {}",&n,&m,&fsn,&fsn_is_even,&fsn_is_odd,&x,&y);
-    return (x, y);
+    (x, y)
 }
 
-fn manhattan_spiral_distance(n0: &i32) -> i32 {
-    let (x,y) = spiral_xy(&n0);
+fn manhattan_spiral_distance(n0: i32) -> i32 {
+    let (x,y) = spiral_xy(n0);
     println!("{:?} -> {:?},{:?}",n0,&x,&y);
-    return x.abs()+y.abs();
+    x.abs()+y.abs()
 }
 
 fn neighbor_sum(grid: &[i32], x: &i32, y: &i32) -> i32 {
     // just never go out of range okay?  :-)
-    return grid[grid_index(&(x-1),&(y-1))] + grid[grid_index(&x,&(y-1))] + grid[grid_index(&(x+1),&(y-1))] +
-        grid[grid_index(&(x-1),&y)] + grid[grid_index(&(x+1),&y)] +
-        grid[grid_index(&(x-1),&(y+1))] + grid[grid_index(&x,&(y+1))] + grid[grid_index(&(x+1),&(y+1))];
+    grid[grid_index(&(x-1),&(y-1))] + grid[grid_index(x,&(y-1))] + grid[grid_index(&(x+1),&(y-1))] +
+        grid[grid_index(&(x-1),y)] + grid[grid_index(&(x+1),y)] +
+        grid[grid_index(&(x-1),&(y+1))] + grid[grid_index(x,&(y+1))] + grid[grid_index(&(x+1),&(y+1))]
 }
 
 // ======================================================================
@@ -351,13 +347,13 @@ fn day04() {
     let test_strings = vec![ "aa bb cc dd ee", "aa bb cc dd aa", "aa bb cc dd aaa"];
     let test_answers = vec![             true,            false,             true];
     for (i,s) in test_strings.iter().enumerate() {
-        let a = valid_passphrase(&s);
+        let a = valid_passphrase(s);
         let a_correct = test_answers[i];
         print!("  valid?({:?}) == {:?}",&s,&a);
         if a != a_correct {
             println!(" FAIL");
         } else {
-            println!("");
+            println!();
         }
     }
 
@@ -382,13 +378,13 @@ fn day04() {
                               "iiii oiii ooii oooi oooo", "oiii ioii iioi iiio"];
     let test_answers = vec![ true, false, true, true, false];
     for (i,s) in test_strings.iter().enumerate() {
-        let a = new_valid_passphrase(&s);
+        let a = new_valid_passphrase(s);
         let a_correct = test_answers[i];
         print!("  valid?({:?}) == {:?}",&s,&a);
         if a != a_correct {
             println!(" FAIL");
         } else {
-            println!("");
+            println!();
         }
     }
 
@@ -408,7 +404,7 @@ fn day04() {
 }
 
 fn valid_passphrase(phrase: &str) -> bool {
-    let split = phrase.split(" ");
+    let split = phrase.split(' ');
     let mut words = HashSet::new();
     for s in split {
         if words.contains(s) {
@@ -419,11 +415,11 @@ fn valid_passphrase(phrase: &str) -> bool {
     if words.len() == 1 {
         return false;
     }
-    return true;
+    true
 }
 
 fn new_valid_passphrase(phrase: &str) -> bool {
-    let split = phrase.split(" ");
+    let split = phrase.split(' ');
     let mut words = HashSet::new();
     let mut sorted_words = HashSet::new();
     for s in split {
@@ -444,7 +440,7 @@ fn new_valid_passphrase(phrase: &str) -> bool {
     if sorted_words.len() == 1 {
         return false;
     }
-    return true;
+    true
 }
 
 // ======================================================================
@@ -463,7 +459,7 @@ fn day05() {
     if steps != 5 {
         println!(" FAIL");
     } else {
-        println!("");
+        println!();
     }
 
     println!("Solution...");
@@ -487,7 +483,7 @@ fn day05() {
     if steps != 10 {
         println!(" FAIL");
     } else {
-        println!("");
+        println!();
     }
 
     println!("Solution...");
@@ -515,7 +511,7 @@ fn sim_cpu_jumps(jumps: &mut Vec<i32>) -> i32 {
         //println!("steps {} addr {}",steps,addr);
         //println!("steps {} addr {} jumps {:?}",steps,addr,jumps);
     }
-    return steps;
+    steps
 }
 
 fn sim_cpu_jumps2(jumps: &mut Vec<i32>) -> i32 {
@@ -524,17 +520,14 @@ fn sim_cpu_jumps2(jumps: &mut Vec<i32>) -> i32 {
     while addr >=0 && addr < jumps.len() as i32 {
         let jump = jumps[addr as usize];
         let next_addr = addr + jump;
-        let mut update = 1;
-        if jump >= 3 {
-            update = -1;
-        }
+        let update = if jump >= 3 { -1 } else { 1 };
         jumps[addr as usize] += update;
         addr = next_addr;
         steps += 1;
         //println!("steps {} addr {}",steps,addr);
         //println!("steps {} addr {} jumps {:?}",steps,addr,jumps);
     }
-    return steps;
+    steps
 }
 
 // ======================================================================
@@ -553,7 +546,7 @@ fn day06() {
     if steps != 5 {
         println!(" FAIL");
     } else {
-        println!("");
+        println!();
     }
 
     println!("Solution...");
@@ -567,12 +560,10 @@ fn day06() {
     let mut test_inputs = vec![ 0, 2, 7, 0];
     let (steps,repeat_steps) = sim_mem_alloc2(&mut test_inputs);
     println!("  steps = {:?} repeat_steps = {:?}",&steps,&repeat_steps);
-    if steps != 5 {
-        println!(" FAIL");
-    } else if repeat_steps != 4 {
+    if (steps != 5) || (repeat_steps != 4) {
         println!(" FAIL");
     } else {
-        println!("");
+        println!();
     }
 
     println!("Solution...");
@@ -591,7 +582,7 @@ fn sim_mem_alloc(block_state: &mut Vec<i32>) -> i32 {
         steps += 1;
         //println!("steps {} state {:?}",steps,block_state);
     }
-    return steps;
+    steps
 }
 
 fn sim_mem_alloc2(block_state: &mut Vec<i32>) -> (i32,i32) {
@@ -611,7 +602,7 @@ fn sim_mem_alloc2(block_state: &mut Vec<i32>) -> (i32,i32) {
     //    None => { println!("unhandled error!"); return (steps,-1); }
     //    Some(x) => { return (steps,steps-*x); }
     //}
-    return (steps,steps-last_step);
+    (steps,steps-last_step)
 }
 
 fn realloc_blocks(block_state: &mut Vec<i32>) {
@@ -626,19 +617,19 @@ fn realloc_blocks(block_state: &mut Vec<i32>) {
     }
 }
 
-fn max_index(v: &Vec<i32>) -> i32 {
+fn max_index(v: &[i32]) -> i32 {
     // find max index & return smallest if multiple matches
     // (stdlib finds last value)
     // not the greatest code.  FIXME
     let mx = v.iter().max();
     match mx {
-        None => { println!("unhandled error!"); return -1; }
+        None => { println!("unhandled error!"); -1 }
         Some(mx) => {
             // now find index with that value
             let mxi = v.iter().position(|&x| x == *mx);
             match mxi {
-                None => { println!("unhandled error!"); return -1; }
-                Some(mxi) => {return mxi as i32;}
+                None => { println!("unhandled error!"); -1 }
+                Some(mxi) => { mxi as i32 }
             }
         }
     }
